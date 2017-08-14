bf61a8a030dcc29b3b0981b30148118f406e09ff
Vincent Li <vincentli@Vincents-MacBook-Air.local>
2017-08-14 13:56:31 +0100

    updated party data and csv

--- src/main/java/com/gcp/poc/f2b/generator/helpers/ModelHelper.java
+++ src/main/java/com/gcp/poc/f2b/generator/helpers/ModelHelper.java
@@ -1,7 +1,7 @@
 package com.gcp.poc.f2b.generator.helpers;
 
 import com.fasterxml.jackson.xml.XmlMapper;
-import com.gcp.poc.f2b.generator.Main;
+import com.gcp.poc.f2b.generator.model.Book;
 import com.gcp.poc.f2b.generator.model.Party;
 
 import java.io.File;
@@ -36,13 +36,40 @@ public class ModelHelper {
         return model;
     }
 
+    public String generateBookCSV() throws IOException {
+        StringBuilder csv = new StringBuilder();
+
+        csv.append("BookId,BookName,BusinessLine,Desk,PrimaryTrader,EntityId,Location,Region\n");
+        List<Book> books = loadModel("books", Book.class);
+        books.forEach(book -> {
+            csv.append(String.format("%s,%s,%s,%s,%s,%s\n",
+                    book.getBookId(),
+                    book.getBookName(),
+                    book.getBusinessLine(),
+                    book.getDesk(),
+                    book.getPrimaryTrader(),
+                    book.getEntityId(),
+                    book.getTradingLocation(),
+                    book.getTradingRegion()));
+        });
+
+        return csv.toString();
+    }
+
     public String generatePartyCSV() throws IOException {
         StringBuilder csv = new StringBuilder();
 
-        csv.append("PartyId,PartyBIC\n");
+        csv.append("PartyId,PartyBIC,LegalName,VerificationStatus,EntityStatus,EffectiveFrom,CountryOfHeadOffice\n");
         List<Party> parties = loadModel("parties", Party.class);
         parties.forEach(party -> {
-            csv.append(party.getBody().getMain().getPartyId()+","+party.getBody().getMain().getPartyBIC()+"\n");
+            csv.append(String.format("%s,%s,%s,%s,%s,%s,%s\n",
+                    party.getBody().getMain().getPartyId(),
+                    party.getBody().getMain().getPartyBIC(),
+                    party.getBody().getLegalName(),
+                    party.getBody().getMain().getVerificationStatus(),
+                    party.getBody().getMain().getEntityStatus(),
+                    party.getBody().getMain().getEffectiveFrom(),
+                    party.getBody().getCountryOfHeadOffice()));
         });
 
         return csv.toString();
--- src/test/java/com/gcp/poc/f2b/generator/helpers/ModelHelperTest.java
+++ src/test/java/com/gcp/poc/f2b/generator/helpers/ModelHelperTest.java
@@ -1,6 +1,5 @@
 package com.gcp.poc.f2b.generator.helpers;
 
-import com.sun.tools.internal.xjc.model.Model;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,5 +9,11 @@ public class ModelHelperTest {
     public void generatePartyCSV() throws IOException {
         ModelHelper modelHelper = new ModelHelper();
         System.out.println(modelHelper.generatePartyCSV());
+    }
+
+    @Test
+    public void generateBookCSV() throws IOException {
+        ModelHelper modelHelper = new ModelHelper();
+        System.out.println(modelHelper.generateBookCSV());
     }
 }
